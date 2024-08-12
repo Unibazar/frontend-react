729dcd6c50c2c8feec7b1d8e9614f4051e42f6a4
Ekta Chaudhary <ektachaudhary129@gmail.com>
2024-08-12 21:48:27 +0530

    Added: pages dir

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,7 @@
-import ContactUs from './components/AppContactUs/ContactUs.js';
 import Footer from './components/Footer/Footer.js';
-import JoinUs from './components/JoinUsSection/JoinUs.js';
-import MeetFounder from './components/MeetFounderSection/MeetFounder.js';
 export default function Home() {
   return (
     <div>
-      <MeetFounder />
-      <JoinUs />
-      <ContactUs />
       <Footer />
     </div>
   );
