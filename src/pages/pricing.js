3a3335a6cb40611aaf5685b5048b8da9388bbf22
utkarsh yadav <utky24@gmail.com>
2024-08-23 17:20:42 +0530

    Complete pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -2,6 +2,9 @@ import Testimonials from "../components/Testimonials/Testimonials"
 import Signup from "../components/Signup/Signup";
 import FAQ from "../components/FAQ/FAQ";
 import Head from 'next/head';
+import Plan from "../components/Plan/Plan";
+import ChoosePlan from "../components/ChoosePlan/ChoosePlan";
+import ComparePlan from "../components/ComparePlan/ComparePlan";
 
 export default function Pricing() {
   return (
@@ -10,6 +13,9 @@ export default function Pricing() {
         <title>Pricing | Unibazar</title>
         <meta name="description" content="Unibazar is online platform which is used to sell your products from various E-commerce platforms." key="desc" />
       </Head>
+      <Plan/>
+      <ChoosePlan/>
+      <ComparePlan/>
       <Testimonials/>
       <FAQ/>
       <Signup/>
