ba8123c4daa390254fa3cc47638f396949acdd09
Bhupendra Sinha <bhupendrasinha35413@gmail.com>
2024-08-26 14:02:35 +0530

    add comment on about-us page

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -16,7 +16,8 @@ export default function AboutUs() {
         <meta name="description" content="Unibazar is online platform which is used to sell your products from various E-commerce platforms." key="desc" />
       </Head>
       {/* <Navbar /> */}
-      <Aboutus />
+      {/* hii changed */}
+        <Aboutus />
       <Enterprises />
       <Story />
       <Ethics />
