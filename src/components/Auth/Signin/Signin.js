68864b74db4c5c063399bbdbc352ca89f3772299
Ekta Chaudhary <ektachaudhary129@gmail.com>
2024-08-27 21:00:23 +0530

    Fixed: &apos; errors

--- src/components/Auth/Signin/Signin.js
+++ src/components/Auth/Signin/Signin.js
@@ -7,7 +7,7 @@ import AppleImg from '../../../assets/apple.png';
 import GoogleImg from '../../../assets/google.png';
 import { IoMdEyeOff, IoMdEye } from 'react-icons/io';
 import Link from 'next/link';
-import { IoChevronBackOutline } from "react-icons/io5";
+import { IoChevronBackOutline } from 'react-icons/io5';
 import { useRouter } from 'next/router';
 
 import styles from './SignIn.module.css';
@@ -21,7 +21,7 @@ const Signin = () => {
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
 
-  const handlePasswordChange = (e) => {
+  const handlePasswordChange = e => {
     setPassword(e.target.value);
   };
 
@@ -32,11 +32,13 @@ const Signin = () => {
   return (
     <>
       <div>
-        <div className='md:hidden pt-12 pl-10  w-full flex'>
-          <IoChevronBackOutline className='bg-gray-50 rounded-full w-8 h-8 p-2 justify-center justify-items-center items-center' onClick={handleBackClick} />
+        <div className="md:hidden pt-12 pl-10  w-full flex">
+          <IoChevronBackOutline className="bg-gray-50 rounded-full w-8 h-8 p-2 justify-center justify-items-center items-center" onClick={handleBackClick} />
         </div>
         <div className={`${styles.logo} pt-12 pl-12`}>
-          <Link href='/'><Image src={LogoImage} alt="Logo" className='w-36' /></Link>
+          <Link href="/">
+            <Image src={LogoImage} alt="Logo" className="w-36" />
+          </Link>
         </div>
       </div>
       <div className="w-full flex md:flex-row flex-col justify-center justify-items-center items-start p-10">
@@ -45,32 +47,29 @@ const Signin = () => {
         </div>
         <div className=" md:mr-10 md:pr-28  w-full p-5">
           <h1 className="text-3xl font-bold text-center">Sign In</h1>
-          <p className='w-full text-normal text-gray-500 text-center py-3'>Please sign in to continue our app</p>
+          <p className="w-full text-normal text-gray-500 text-center py-3">Please sign in to continue our app</p>
           <form className="flex flex-col justify-center items-center gap-3">
             <input type="text" placeholder="Email" className="w-full p-2 mb-5 border bg-gray-100 rounded-2xl " />
-            <div className='w-full flex flex-row justify-between p-2  border bg-gray-100 rounded-2xl'>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                value={password}
-                onChange={handlePasswordChange}
-                placeholder="Password"
-                className="w-auto outline-0 border-0 bg-gray-100"
-              />
-              <div className="w-auto items-center justify-center justify-items-center text-center pt-1">
-                {showPassword ? (
-                  <IoMdEye style={{ color: 'gray' }} className='cursor-pointer' onClick={handleTogglePasswordVisibility} />
-                ) : (
-                  <IoMdEyeOff style={{ color: 'gray' }} className='cursor-pointer'  onClick={handleTogglePasswordVisibility} />
-                )}
-              </div>
+            <div className="w-full flex flex-row justify-between p-2  border bg-gray-100 rounded-2xl">
+              <input type={showPassword ? 'text' : 'password'} value={password} onChange={handlePasswordChange} placeholder="Password" className="w-auto outline-0 border-0 bg-gray-100" />
+              <div className="w-auto items-center justify-center justify-items-center text-center pt-1">{showPassword ? <IoMdEye style={{ color: 'gray' }} className="cursor-pointer" onClick={handleTogglePasswordVisibility} /> : <IoMdEyeOff style={{ color: 'gray' }} className="cursor-pointer" onClick={handleTogglePasswordVisibility} />}</div>
             </div>
-            <p className='w-full text-normal text-blue-500 text-right p-1 pb-2'><Link href='/forgot-password' >Forget Password?</Link></p>
+            <p className="w-full text-normal text-blue-500 text-right p-1 pb-2">
+              <Link href="/forgot-password">Forget Password?</Link>
+            </p>
             <button className="bg-teal-500 w-full hover:bg-teal-700 text-white font-bold  px-4 rounded-2xl py-3">Sign In</button>
-            <p className='w-full text-gray-600 py-5 text-center'>Don't have an account ? <Link href='/register' className='text-blue-500'>Sign up</Link><br/>Or connect</p>
-            <div className='w-full flex md:flex-row items-center justify-center justify-items-center gap-1 py-3'>
-              <Image src={FbImg} alt='facbook-image'></Image>
-              <Image src={AppleImg} alt='facbook-image'></Image>
-              <Image src={GoogleImg} alt='facbook-image'></Image>
+            <p className="w-full text-gray-600 py-5 text-center">
+              Don&apos;t have an account ?{' '}
+              <Link href="/register" className="text-blue-500">
+                Sign up
+              </Link>
+              <br />
+              Or connect
+            </p>
+            <div className="w-full flex md:flex-row items-center justify-center justify-items-center gap-1 py-3">
+              <Image src={FbImg} alt="facbook-image"></Image>
+              <Image src={AppleImg} alt="facbook-image"></Image>
+              <Image src={GoogleImg} alt="facbook-image"></Image>
             </div>
           </form>
         </div>
--- src/components/contact-us/ContactSupport/ContactSupport.js
+++ src/components/contact-us/ContactSupport/ContactSupport.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import Image from 'next/image'
-import ContactSupportImg from '../../../assets/unibazar-contactus-images/contactSupport.svg'
-import Link from 'next/link'
+import React from 'react';
+import Image from 'next/image';
+import ContactSupportImg from '../../../assets/unibazar-contactus-images/contactSupport.svg';
+import Link from 'next/link';
 const ContactSupport = () => {
   return (
     <>
-      <div className='w-full flex md:flex-row flex-col content-center justify-between p-5'>
-        <div className='p-10 pt-14'>
-          <p className='font-bold text-3xl'>GET IN TOUCH WITH US</p>
-          <p className='text-lg py-3 pb-8'>We're Here To Help You With Any Questions Or <br/>Support You Need</p>
-          <Link href="#" className="px-4 py-2 text-white bg-teal-700 rounded hover:bg-teal-800 ">Contact Support</Link>
+      <div className="w-full flex md:flex-row flex-col content-center justify-between p-5">
+        <div className="p-10 pt-14">
+          <p className="font-bold text-3xl">GET IN TOUCH WITH US</p>
+          <p className="text-lg py-3 pb-8">
+            We&apos;re Here To Help You With Any Questions Or <br />
+            Support You Need
+          </p>
+          <Link href="#" className="px-4 py-2 text-white bg-teal-700 rounded hover:bg-teal-800 ">
+            Contact Support
+          </Link>
         </div>
-        <Image src={ContactSupportImg} alt='wait' className='p-5'></Image>
+        <Image src={ContactSupportImg} alt="wait" className="p-5"></Image>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ContactSupport
+export default ContactSupport;
