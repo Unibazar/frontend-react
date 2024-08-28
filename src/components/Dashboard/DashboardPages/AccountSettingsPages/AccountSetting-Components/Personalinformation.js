d0a7698562789923a2d3e560e2fdb9f35585c980
dilip <mihirsingh241@gmail.com>
2024-08-28 14:30:09 +0530

    created states for handling form data in accountSettings/Personalinformation

--- src/components/Dashboard/DashboardPages/AccountSettingsPages/AccountSetting-Components/Personalinformation.js
+++ src/components/Dashboard/DashboardPages/AccountSettingsPages/AccountSetting-Components/Personalinformation.js
@@ -10,10 +10,28 @@ export default function Personalinformation() {
   const [userPhotoSrc, setUserPhotoSrc] = useState("");
   const [userPhoto, setUserPhoto] = useState();
   const userPhotoRef = useRef();
+  
+  const [userData, setUserData] = useState({
+    userName: "",
+    phoneNumber: "",
+    location: "",
+    email: "",
+    bio: "",
+  });
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData(prevState => ({ ...prevState, [name]: value }))
+  }
 
   const handleSubmit = (e)=>{
       e.preventDefault();
+      console.log(userData);
+      console.log(userPhoto);
+      
   }
+
 
   return (
     <div>
@@ -32,27 +50,27 @@ export default function Personalinformation() {
           <div className="flex gap-7 flex-wrap">
             <div className="flex flex-1 flex-col gap-2">
               <label htmlFor="userName">Full Name:</label>
-              <input id="userName" type="text" placeholder="Add Product Name" name="userName" className="border-2 text-sm md:text-lg p-2 rounded-lg" />
+              <input id="userName" type="text" placeholder="Add Product Name" name="userName" value={userData.userName} onChange={(e) => handleChange(e)} className="border-2 text-sm md:text-lg p-2 rounded-lg" />
             </div>
             <div className="flex flex-1 flex-col gap-2">
               <label htmlFor="phoneNumber">Phone Number:</label>
-              <input id="phoneNumber" type="number" placeholder="Phone" name="phoneNumber" className="border-2 text-sm md:text-lg p-2 rounded-lg" />
+              <input id="phoneNumber" type="number" placeholder="Phone" name="phoneNumber" value={userData.phoneNumber} onChange={(e) => handleChange(e)}  className="border-2 text-sm md:text-lg p-2 rounded-lg" />
             </div>
           </div>
           <div className="flex gap-7 flex-wrap mt-5">
             <div className="flex flex-1 flex-col gap-2">
               <label htmlFor="location">Location:</label>
-              <input id="location" type="text" placeholder="Enter Address" name="location" className="border-2 text-sm md:text-lg p-2 rounded-lg" />
+              <input id="location" type="text" placeholder="Enter Address" name="location" value={userData.location} onChange={(e) => handleChange(e)} className="border-2 text-sm md:text-lg p-2 rounded-lg" />
             </div>
             <div className="flex flex-1 flex-col gap-2">
               <label htmlFor="email">Email:</label>
-              <input id="email" type="email" placeholder="Enter Your Email" name="email" className="border-2 text-sm md:text-lg p-2 rounded-lg" />
+              <input id="email" type="email" placeholder="Enter Your Email" name="email" value={userData.email} onChange={(e) => handleChange(e)} className="border-2 text-sm md:text-lg p-2 rounded-lg" />
             </div>
           </div>
 
           <div className="flex flex-col gap-2 flex-1 w-full mt-7">
             <label htmlFor="bio">Biography:</label>
-            <textarea id="bio" placeholder="Write Something About Yourself" name="bio" rows={6} className="border-2 text-sm md:text-lg p-2 rounded-lg"></textarea>
+            <textarea id="bio" placeholder="Write Something About Yourself" name="bio" rows={6} value={userData.bio} onChange={(e) => handleChange(e)} className="border-2 text-sm md:text-lg p-2 rounded-lg"></textarea>
           </div>
 
 
