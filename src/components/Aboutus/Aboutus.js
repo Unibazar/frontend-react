e2ee946e864bde9c934b55c85a2d6242223ddf9e
utkarsh yadav <utky24@gmail.com>
2024-08-30 12:29:16 +0530

    fix Aboutus Layout css

--- src/components/Aboutus/Aboutus.js
+++ src/components/Aboutus/Aboutus.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import Image from 'next/image';
 import AboutusImage from '../../assets/unibazar-aboutus-images/aboutus.svg';
-import styles from './AboutUs.module.css';
+import Link from 'next/link'
 const Aboutus = () => {
   return (
     <>
-      <div className=" h-1/2 w-full flex md:flex-row flex-col items-start p-5">
-        <div className=" w-full flex md:flex-row flex-col justify-between p-5 ">
-          <div className="md:p-5">
-            <h1 className={styles.heading}>ABOUT US</h1>
-            <p className="font-light text-1xl pb-4 pt-4">
-              Discover how Unibazar can transform your <br /> multi-platform selling experience.
-            </p>
-            <button className="bg-teal-600 px-4 py-2 text-white text-xl rounded" type="button">
-              Get Start for Free
-            </button>
+
+<div className=" w-full flex md:flex-row flex-col justify-between items-center  md:mt-5 ">
+        <div className=" w-full justify-items-end ">
+          <h1 className="w-full font-bold uppercase text-4xl p-3">
+          ABOUT US
+          </h1>
+          <p className="w-full capitalize text-xl font-thin mb-4 p-3">
+          Discover how Unibazar can transform your <br /> multi-platform selling experience.
+          </p>
+          <div className="pt-3 p-3">
+            <Link href="/register" className="px-12 py-4 text-white text-lg font-thin bg-teal-500 rounded-xl hover:bg-teal-600 ">
+            Get Start for Free
+            </Link>
           </div>
-          <Image src={AboutusImage} alt="wait"></Image>
+          <br />
+          <br />
+        </div>
+
+        <div className=" md:w-4/5 w-full md:pl-16 justify-center md:justify-right justify-items-center items-center">
+          <Image src={AboutusImage} alt="hero-image.."></Image>
         </div>
       </div>
     </>
--- src/components/JoinUsSection/JoinUs.js
+++ src/components/JoinUsSection/JoinUs.js
@@ -5,13 +5,16 @@ import Link from 'next/link';
 const Joinus = () => {
   return (
     <>
-      <div className='w-full flex md:flex-row flex-col justify-around items-start md:pr-36 pt-11 p-5'>
-        <div className=' md:w-2/3 p-5'>
-         <p className="font-bold">JOIN US</p>
-         <p className="py-2.5 font-normal">Join the growing community of successful sellers who have chosen Unibazar to enhance their e-commerce operations. Together, we can transform the way you manage your online business.</p>
-         <button type='button' className=' px-4 py-2 text-white bg-teal-700 rounded hover:bg-teal-800'><Link href='/register'>Signup for free</Link></button>
+      <div className='w-full flex md:flex-row flex-col justify-around items-start md:pr-36 pt-10 pb-10 md:p-10'>
+        <div className=' md:w-[550px] p-5'>
+         <p className="font-bold text-xl">JOIN US</p>
+         <p className="py-2.5 font-normal text-base text-gray-500">Join the growing community of successful sellers who have chosen Unibazar to enhance their e-commerce operations. Together, we can transform the way you manage your online business.</p>
+         <button type='button' className=' px-12 text-xl font-thin py-4 text-white bg-teal-500 rounded-xl hover:bg-teal-600 mt-12'><Link href='/register'>Signup for free</Link></button>
         </div>
+        <div className='md:w-60 flex justify-center'>
         <Image src={image} alt="join us "></Image>
+        </div>
+        
       </div>
     </>
   );
