d7f237b3bddaf64bd13ec28d0dc74986343294d9
dilip <mihirsingh241@gmail.com>
2024-09-04 18:08:19 +0530

    navbar issue resolve

--- src/components/common/AppNavBar.js
+++ src/components/common/AppNavBar.js
@@ -44,40 +44,41 @@ function AppNavBar() {
   };
 
   return (
-    <nav className={styles.navbarContainer}>
-      <Link href="/" className={styles.logoWrap}>
-        <Image src={LogoImage} alt="logo" className={styles.navbarLogo}></Image>
+    <nav className={`flex justify-between items-center py-5 px-4 md:px-16 border-b-2 ${user && "flex-row-reverse md:flex-row"} `}>
+      <Link href="/" className={`w-[170px] md:w-[200px] ${user && "hidden md:inline"}`}>
+        <Image src={LogoImage} alt="logo" className='w-full h-full'></Image>
       </Link>
 
       <div className="hidden md:flex space-x-6 text-gray-600">
-        <ul className={styles.navLinksUl}>
-          <li className={styles.navLinkItem}>
+        <ul className="flex gap-4">
+          <li className="text-lg py-2">
             <Link href="/" className={isActive('/') ? ' text-teal-500' : 'text-gray-600'}>
               <span>Home</span>
             </Link>
           </li>
-          <li className={styles.navLinkItem}>
+          <li className="text-lg py-2">
             <Link href="/about-us" className={isActive('/about-us') ? ' text-teal-500' : 'text-gray-600'}>
               About
             </Link>
           </li>
-          <li className={styles.navLinkItem}>
+          <li className="text-lg py-2">
             <Link href="/features" className={isActive('/features') ? ' text-teal-500' : 'text-gray-600'}>
               Features
             </Link>
           </li>
-          <li className={styles.navLinkItem}>
+          <li className="text-lg py-2">
             <Link href="/pricing" className={isActive('/pricing') ? ' text-teal-500' : 'text-gray-600'}>
               Pricing
             </Link>
           </li>
-          <li className={styles.navLinkItem}>
+          <li className="text-lg py-2">
             <Link href="/contact-us" className={isActive('/contact-us') ? ' text-teal-500' : 'text-gray-600'}>
               Contact us
             </Link>
           </li>
         </ul>
       </div>
+      
       {user ? (
         <div className={styles.profileContainer} onClick={toggleDropdown}>
           <Image className={styles.profileImg} src={profileImg} alt="Profile Image" width={40} height={40} />
@@ -93,8 +94,8 @@ function AppNavBar() {
           )}
         </div>
       ) : (
-        <div className={styles.navButtons}>
-          <Link href="/login" className={styles.loginButton}>
+        <div className="hidden gap-4 items-center md:flex">
+          <Link href="/login" className="px-5 py-2 rounded-md bg-zinc-200">
             Login
           </Link>
           <Link href="/register" className="px-4 py-2 text-white bg-teal-700 rounded hover:bg-teal-800">
@@ -104,11 +105,11 @@ function AppNavBar() {
       )}
 
       <Button onClick={toggleDrawer(true)} className="text-black md:hidden">
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-6 h-6 md:hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={DrawerOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}></path>
         </svg>
       </Button>
-      <Drawer anchor={'right'} className="md:hidden" open={DrawerOpen} onClose={toggleDrawer(false)}>
+      <Drawer anchor={`${user?"left":"right"}`} className="md:hidden" open={DrawerOpen} onClose={toggleDrawer(false)}>
         <DrawerList toggleDrawer={toggleDrawer} isActive={isActive} />
       </Drawer>
     </nav>
