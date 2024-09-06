92e5705b89cda10f2f5d44a0c219ac59e116c2e6
dilip <mihirsingh241@gmail.com>
2024-09-06 20:47:01 +0530

    add a dummy data for card and improve the ui

--- src/components/Dashboard/DashboardPages/ProductsPages/ProductPage-Components/Card.js
+++ src/components/Dashboard/DashboardPages/ProductsPages/ProductPage-Components/Card.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import StarIcon from '@mui/icons-material/Star';
 
-export default function Card() {
+export default function Card({ item }) {
   return (
     <div class="flex flex-col text-gray-700 bg-white overflow-hidden rounded-sm border-2 max-w-[270px]">
+      {/* image part  */}
       <div class="text-white">
-        <img src="https://img.freepik.com/free-photo/3d-beauty-product-studio_23-2151401472.jpg" alt="card-image" className="w-full h-full object-cover" />
+        <img src={item.imageUrl} alt="card-image" className="w-full h-full object-cover" />
       </div>
-      <div class="py-3 px-4">
-        <div class="mb-2 flex text-yellow-500">
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <p className="text-black">43</p>
+
+      {/* description part  */}
+      <div class="py-3 px-4 flex flex-col justify-between h-full">
+        <div className='header'>
+          <div class="mb-2 flex text-yellow-500">
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <p className="text-black">{item.reviews}</p>
+          </div>
+          <p class="block font-sans text-base">{item.description}k</p>
         </div>
-        <p class="block font-sans text-base">The place is close to Barceloneta Beach and bus stop just 2 min by walk</p>
-        <div class="flex justify-between items-center pt-2">
-          <p className="text-blue-500">$342</p>
+        <div class="footer flex justify-between items-center pt-2 ">
+          <p className="text-blue-500">{item.price}</p>
           <button class=" py-1 px-4 rounded-sm bg-zinc-200 text-black font-semibold" type="button">
             Edit
           </button>
--- src/components/Dashboard/DashboardPages/ProductsPages/ProductPage-Components/CardLayout.js
+++ src/components/Dashboard/DashboardPages/ProductsPages/ProductPage-Components/CardLayout.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Card from './Card'
+import Card from './Card';
+import CardsData from '../DummyData/CardsData';
 
 export default function CardLayout() {
     return (
@@ -8,11 +9,9 @@ export default function CardLayout() {
                 <h1 className='font-semibold'>17 OCT, 2024</h1>
             </div>
             <div className='cardContiner py-4 flex gap-4 flex-wrap px-2'>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {CardsData.map(item=>(
+                    <Card key={item.id} item={item}/>
+                ))}
             </div>
         </div>
     )
