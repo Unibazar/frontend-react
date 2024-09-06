458214e71460cda4d494e4df0f0ed90b9cf700e2
dilip <mihirsingh241@gmail.com>
2024-09-06 20:59:32 +0530

    sovle a small issue

--- src/components/Dashboard/DashboardPages/ProductsPages/ProductPage-Components/Card.js
+++ src/components/Dashboard/DashboardPages/ProductsPages/ProductPage-Components/Card.js
@@ -2,17 +2,18 @@ import React from 'react';
 import StarIcon from '@mui/icons-material/Star';
 
 export default function Card({ item }) {
+  
   return (
     <div class="flex flex-col text-gray-700 bg-white overflow-hidden rounded-sm border-2 max-w-[270px]">
       {/* image part  */}
       <div class="text-white">
-        <img src={item.imageUrl} alt="card-image" className="w-full h-full object-cover" />
+        <img src={item.imageUrl} alt="card-image" className="w-full h-[200px] object-contain" />
       </div>
 
       {/* description part  */}
       <div class="py-3 px-4 flex flex-col justify-between h-full">
         <div className='header'>
-          <div class="mb-2 flex text-yellow-500">
+          <div className="mb-2 flex text-yellow-500">
             <StarIcon />
             <StarIcon />
             <StarIcon />
@@ -20,7 +21,7 @@ export default function Card({ item }) {
             <StarIcon />
             <p className="text-black">{item.reviews}</p>
           </div>
-          <p class="block font-sans text-base">{item.description}k</p>
+          <p class="block font-sans text-base">{item.description.slice(0,50)}...</p>
         </div>
         <div class="footer flex justify-between items-center pt-2 ">
           <p className="text-blue-500">{item.price}</p>
