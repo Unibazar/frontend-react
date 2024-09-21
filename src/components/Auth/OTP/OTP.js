3486aef08ddd5ecaeb954f35f4bbeb6bad852484
utkarsh yadav <utky24@gmail.com>
2024-09-22 05:21:09 +0530

    update otp focus issue, update resetpass layout & input

--- src/components/Auth/OTP/OTP.js
+++ src/components/Auth/OTP/OTP.js
@@ -1,54 +1,147 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Image from 'next/image';
 import SigninImg from '../../../assets/signin-image.svg';
 import LogoImage from '../../../assets/unibazar-home-images/unibazarlogo.png';
-import Link from 'next/link';
 import { IoChevronBackOutline } from 'react-icons/io5';
+import Link from 'next/link';
+import { useSelector, useDispatch } from 'react-redux';
+import styles from './Otp.module.css';
 import { useRouter } from 'next/router';
+import { otpVerification } from '@/redux/slice/userSlice';
+import Loader from '../../../Loader/Loader';
+import { toast } from 'react-toastify';
 
-import styles from './OTP.module.css';
-const OTP = () => {
+const Otp = () => {
+  const [inputs, setinputs] = useState(new Array(4).fill(""));
+  const [inputArr, setInputArr] = useState(inputs)
+  const [msg, setMsg] = useState(false)
+
+  const { user, isLoading, error } = useSelector(state => state.user);
+  const [otp, setOtp] = useState(['', '', '', '']);
+  const [timeLeft, setTimeLeft] = useState(120); // 120 seconds for 2 minutes
+  const [canResend, setCanResend] = useState(false); // State to control the resend button
   const router = useRouter();
+  const dispatch = useDispatch();
+
+
+
   const handleBackClick = () => {
     router.back();
   };
-  const [otp, setOtp] = useState(['', '', '', '']);
-  const [email, setEmail] = useState('uihut@gmail.com');
-  const [time, setTime] = useState(60); // 1 minute
-  const [resend, setResend] = useState(false);
 
   useEffect(() => {
-    let timerId;
-    if (time > 0) {
-      timerId = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else {
-      setResend(true);
+    if (user && user.user.verified) {
+      toast.success('user register successfully!');
+      router.replace('/dashboard');
     }
-    return () => clearTimeout(timerId); // clean up the timer on unmount
-  }, [time]); // re-run the effect when time changes
 
-  const handleOtpChange = (index, value) => {
-    if (value.length > 1) return; // prevent entering more than one character
-    const newOtp = [...otp];
-    newOtp[index] = value;
-    setOtp(newOtp);
+    // console.log(user);
+
+    if (error && error.message) {
+      toast.error(error.message);
+    }
+  }, [user, error, router]);
+
+
+  const refs = [useRef(), useRef(), useRef(), useRef()];
+
+  const handleOtpChange = (event, index) => {
+    const val = event.target.value;
+    if (!Number(val))
+      return;
+
+    const copyArray = [...inputArr]
+    copyArray[index] = val
+    setInputArr(copyArray)
+
+    if (index < inputArr.length - 1) {
+      refs[index + 1].current.focus();
+    }
+
+  }
+
+  const handleKey = (event, index) => {
+    if (event.keyCode == 8) {
+      const copyArray = [...inputArr]
+      copyArray[index] = ""
+      setInputArr(copyArray)
+      if (index > 0) {
+        refs[index - 1].current.focus();
+      }
+    }
+    if (index < inputArr.length - 1) {
+      if (event.keyCode == 39) {
+        refs[index + 1].current.focus();
+      }
+    }
+
+    if (index > 0) {
+      if (event.keyCode == 37) {
+        refs[index - 1].current.focus();
+      }
+    }
+  }
+
+  const handlePaste = (event) => {
+    const data = event.clipboardData.getData("text");
+    const dataArray = data.split("")
+    setInputArr(dataArray)
+
+    refs[inputArr.length - 1].current.focus()
+
+  }
+
+
+
+  useEffect(() => {
+    refs[0].current.focus()
+
+  }, [])
+
+
+  {/*const handleOtpChange = (e, index) => {
+    const value = e.target.value;
+    if (/^\d*$/.test(value)) {
+      let newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+      if (value !== '' && index < 3) {
+        document.getElementById(`otp-${index + 1}`).focus();
+      }
+    }
+  };*/}
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    dispatch(otpVerification(otp.join('')));
+
+    // console.log('Entered OTP:', otp.join(''));
   };
 
-  const handleVerify = () => {
-    // Verify OTP logic here
-  };
+  // Timer logic
+  useEffect(() => {
+    if (timeLeft > 0) {
+      const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+      return () => clearTimeout(timer);
+    } else {
+      setCanResend(true);
+    }
+  }, [timeLeft]);
 
   const handleResend = () => {
-    setResend(true);
-    setTime(60); // reset timer
+    //    here will be logic for resend otp
+
+    setTimeLeft(120); // Reset the timer to 2 minutes
+    setCanResend(false); // Disable the resend button
   };
 
   return (
     <>
+      {isLoading && <Loader />}
       <div>
-        <div className="md:hidden pt-12 pl-10  w-full flex">
+        <div className="md:hidden pt-12 pl-10 w-full flex">
           <IoChevronBackOutline className="bg-gray-50 rounded-full w-8 h-8 p-2 justify-center justify-items-center items-center" onClick={handleBackClick} />
         </div>
         <div className={`${styles.logo} pt-12 pl-12`}>
@@ -57,39 +150,46 @@ const OTP = () => {
           </Link>
         </div>
       </div>
-      <div className="w-full flex md:flex-row flex-col justify-center justify-items-center items-start p-10">
-        <div className={`${styles.mainimg} w-full md:ml-20 md:mt-10 p-5 `}>
+      <div className="w-full flex md:flex-row flex-col justify-around p-10">
+        <div className={`${styles.mainimg} w-full min-w-[400px] min-h-[400px] h-full justify-center flex md:mt-10 p-5 `}>
           <Image src={SigninImg} alt="Signin_Image"></Image>
         </div>
-        <div className=" md:mr-10 md:mt-16 md:pr-28  w-full p-5">
+        <div className="flex flex-col justify-center min-w-[250px] w-full p-5">
           <h1 className="text-3xl font-bold text-center">OTP Verification</h1>
-          <p className="w-full text-normal text-gray-500 text-center py-3 pb-5">
-            Please check your email {email} <br />
-            to see the verification code
+          <p className="w-full text-normal text-gray-500 text-center py-3">
+            Please check your email {user && user.user.email} <br /> we have sent OTP
           </p>
-          <form className="flex flex-col justify-center items-center gap-3">
-            <p className="w-full text-2xl md:mt-5 font-bold text-left">OTP Code</p>
-            <div className="w-full flex flex-row justify-around">
-              <input type="text" value={otp[0]} onChange={e => handleOtpChange(0, e.target.value)} placeholder="0" className="w-10 h-10 text-center  rounded  bg-gray-200 " />
-              <input type="text" value={otp[1]} onChange={e => handleOtpChange(1, e.target.value)} placeholder="0" className="w-10 h-10 text-center  rounded  bg-gray-200 " />
-              <input type="text" value={otp[2]} onChange={e => handleOtpChange(2, e.target.value)} placeholder="0" className="w-10 h-10 text-center  rounded  bg-gray-200 " />
-              <input type="text" value={otp[3]} onChange={e => handleOtpChange(3, e.target.value)} placeholder="0" className="w-10 h-10 text-center  rounded  bg-gray-200 " />
+          <form id="otp-form" onSubmit={handleSubmit}>
+            <h1 className="text-xl font-bold w-64 m-auto">OTP Code</h1>
+            <div className="flex  items-center justify-center gap-3 p-2">
+              {inputs.map((curValue, index) => {
+                return (
+                  <input type='text' key={index} className='w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-slate-100 border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-teal-600 focus:ring-2 focus:ring-teal-400' maxLength="1" ref={refs[index]} value={inputArr[index]}
+                    onPaste={(event) => handlePaste(event)}
+                    onKeyDown={(event) => handleKey(event, index)}
+                    onChange={(event) => handleOtpChange(event, index)} />
+                )
+              })}
+
+
+              {/* {[0, 1, 2, 3].map((_, index) => (
+                <input key={index} id={`otp-${index}`} type="text" inputMode="numeric" pattern="\d*" maxLength="1" className="w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-slate-100 border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100" value={otp[index]} onChange={e => handleOtpChange(e, index)} disabled={index > 0 && otp[index - 1] === ''} />
+              ))}*/}
             </div>
-            <button
-              className="bg-teal-500 w-full hover:bg-teal-700 text-white font-bold py-
-            2 px-4 rounded-2xl py-3"
-              onClick={handleVerify}
-            >
-              Verify
-            </button>
-            <div className="w-full justify-between flex flex-row px-2">
-              <p className="w-full text-gray-600 py-2 text-left">Resend code to {email}</p>
-              {resend ? (
-                <button className="bg-teal-500 w-20  hover:bg-teal-700 text-white font-bold  rounded-xl " onClick={handleResend}>
-                  Resend
-                </button>
+            <div className="max-w-[260px] mx-auto mt-4">
+              <button type="submit" className="bg-teal-500 w-full hover:bg-teal-700 text-white font-bold px-4 rounded py-3" >
+                Verify
+              </button>
+            </div>
+            <div className="text-center mt-4">
+              {timeLeft > 0 ? (
+                <p className="text-gray-500">
+                  Resend OTP in {Math.floor(timeLeft / 60)}:{timeLeft % 60 < 10 ? `0${timeLeft % 60}` : timeLeft % 60}
+                </p>
               ) : (
-                <p className="w-20  text-gray-600 py-2 text-right">{time} seconds</p>
+                <button onClick={handleResend} className={`text-teal-500 hover:text-teal-700 ${canResend ? '' : 'opacity-50 cursor-not-allowed'}`} disabled={!canResend}>
+                  Resend OTP
+                </button>
               )}
             </div>
           </form>
@@ -99,4 +199,4 @@ const OTP = () => {
   );
 };
 
-export default OTP;
+export default Otp;
