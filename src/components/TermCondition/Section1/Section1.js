e08c0e5fc531001c162a94a9ed32f2b3fa987536
utkarsh yadav <utky24@gmail.com>
2024-09-26 14:53:41 +0530

    Added t&c page

---
+++ src/components/TermCondition/Section1/Section1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className='w-full flex flex-col justify-center text-center items-center'>
+      <p className='w-full font-bold text-2xl '>Welcome to Unibazar Technologies Pvt. Ltd.!</p>
+      <br />
+      <p className='w-full text-[18px] opacity-70 px-0 md:px-36'>These Terms of Service govern your access to and use of our platform. By accessing or using Unibazar, you agree to these terms. If you do not agree to these terms, please do not use our platform.</p>
+    </div>
+
+  )
+}
+
+export default Section1
