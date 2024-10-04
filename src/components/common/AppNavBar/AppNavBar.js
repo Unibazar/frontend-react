ac7a4d1d9732a0588cd9561af2290420b6bca475
dilip <mihirsingh241@gmail.com>
2024-10-04 10:51:52 +0530

    issue of navbar is resolved

--- src/components/common/AppNavBar/AppNavBar.js
+++ src/components/common/AppNavBar/AppNavBar.js
@@ -121,13 +121,16 @@ function AppNavBar() {
               </Link>
             </div>
         }
-        <div className="md:hidden flex items-center">
+
+
+        {!user && <div className="md:hidden flex items-center">
           <button onClick={toggleMenu} className="text-gray-600 hover:text-teal-700 focus:outline-none">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}></path>
             </svg>
           </button>
-        </div>
+        </div>}
+
         {isOpen && (
           <div className="md:hidden absolute top-16 left-0 w-full bg-white shadow-md">
             <ul className="flex flex-col items-center space-y-4 p-4">
