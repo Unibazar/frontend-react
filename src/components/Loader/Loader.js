f8a3174dd517b33307586933bb8d335ffa96e376
Utkarsh Yadav <165546567+Utkarshya24@users.noreply.github.com>
2024-11-18 19:40:47 +0000

    update loader & about us page

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Loader.module.css';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Loader = () => {
     return (
-            <div className={styles.loading}>
-                <span className={styles.loader}></span>
-            </div>
+        <div className={styles.loading}>
+            <CircularProgress className='text-teal-600' />
+        </div>
     )
 }
 
