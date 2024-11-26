f8d2a9c366b924cf4fd79aaa6269a56262802fd6
utkarsh <utkarshyadav2424@gmail.com>
2024-11-26 14:51:40 +0530

    update business information

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slice/userSlice';
 import productReducer from './slice/productSlice';
+import credentialReducer from './slice/credentialSlice';
 
 
 const store = configureStore({
     reducer: {
         user: userReducer,
-        product: productReducer
+        product: productReducer,
+        credentials: credentialReducer,
     }
 })
 
