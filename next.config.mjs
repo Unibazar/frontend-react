3c40124a0881576fcbfb1c2190b67c7f285cd0c8
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2024-11-26 19:03:51 +0530

    added dcdeploy hosted link

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: ['images.unsplash.com'], // Replace with your actual domains
   },
   publicRuntimeConfig: {
-    API_BASE_URL: process.env.NEXT_MODE === 'development' ? 'http://localhost:4000' : 'https://backend-express-29ku.onrender.com'
+    API_BASE_URL: process.env.NEXT_MODE === 'development' ? 'http://localhost:4000' : 'https://unibazar-4u6g14qyvw.dcdeploy.cloud'
   },
 };
 
