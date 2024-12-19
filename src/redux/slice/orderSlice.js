7d36a181c640498a600df8a1a199517e51d0b919
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2024-12-19 21:19:05 +0530

    added order slice

---
+++ src/redux/slice/orderSlice.js
@@ -0,0 +1,64 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+const apiUrl = publicRuntimeConfig.API_BASE_URL;
+
+axios.defaults.withCredentials = true;
+
+// Thunk to fetch orders dynamically based on user-specific credentials
+export const fetchOrders = createAsyncThunk(
+  "orders/fetch",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const token = localStorage.getItem("jwtToken");
+      const { sellerId, marketplaceId, refreshToken,clientId,clientSecret } = credentials;
+
+      // Make a POST request to fetch orders dynamically
+      const response = await axios.post(
+        `${apiUrl}/api/productList/`,
+        { sellerId, marketplaceId, refreshToken,clientId,clientSecret },
+        {
+          headers: { token },
+          withCredentials: true,
+        }
+      );
+
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response?.data || "Failed to fetch orders, please try again!"
+      );
+    }
+  }
+);
+
+// Orders Slice
+const ordersSlice = createSlice({
+  name: "orders",
+  initialState: {
+    orders: null,
+    isLoading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchOrders.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchOrders.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orders = action.payload;
+        state.error = null;
+      })
+      .addCase(fetchOrders.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default ordersSlice.reducer;
